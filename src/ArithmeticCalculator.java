27781a55f705ed9c5e935a20f6237cda6112cf2a
sebbykim <sebbykim1980@gmail.com>
2024-12-16 16:23:10 +0000

    oop se calculator with calculator (not fucnitonal)

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -8,6 +8,7 @@ public class ArithmeticCalculator extends Calculator {
     }
 
     // Throws Exception which we handle up the stack
+    @Override
     void parse() throws Exception {
 
         // Send the raw input to the parser with order of operations and no special chars
