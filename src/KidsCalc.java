8001e69bbca3d93db7bb608a3c4056022e0850cc
matthewb62 <24416827@studentmail.ul.ie>
2024-12-17 18:48:59 +0000

    finished kids calc stuff

--- src/KidsCalc.java
+++ src/KidsCalc.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class KidsCalc {
     Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         KidsCalc calc = new KidsCalc();
         calc.start();
     }
@@ -18,10 +18,8 @@ public class KidsCalc {
         } while (choice != 0);
     }
 
-    private void kidsMenu()
-
-    {
-        System.out.print("Welcome to Kids Calculator!\n\nEnter '1' to learn addition!");
+    private void kidsMenu() {
+        System.out.print("\nWelcome to Kids Calculator!\n\nEnter '1' to learn addition!");
         System.out.print("\nEnter '2' to learn subtraction!\nEnter '3' to learn division!\n");
         System.out.println("Enter '4' to learn multiplication!\n\nEnter '0' to exit");
     }
@@ -35,11 +33,17 @@ public class KidsCalc {
             return -23;
         }
     }
-
     int num1 = 0;
     int num2 = 0;
     boolean validAddInput = false;
     boolean validAddInput2 = false;
+
+    private void reset() {
+    num1 = 0;
+    num2 = 0;
+    validAddInput = false;
+    validAddInput2 = false;
+}
 
     private int validNumber() {
 
@@ -83,10 +87,8 @@ public class KidsCalc {
         switch (choice){
             case 1:
                 System.out.print("\nAddition selected!\n\n");
-                num1 = 0;
-                num2 = 0;
-                validAddInput = false;
-                validAddInput2 = false;
+
+                reset();
                 validNumber();
                 validNumber2();
                 int addresult = num1 + num2;
@@ -117,7 +119,6 @@ public class KidsCalc {
 
                 int j = 1;
                 while(j<10 && j <= addresult){
-
                     System.out.print("|  " + j + "  | ");
                     j++;}
                 for (int i = 10; i <= addresult; i++) {
@@ -138,21 +139,26 @@ public class KidsCalc {
                     System.out.print("+-----+ ");
                 }
 
-                System.out.println("\n\n As we can see: " + num1 + " + " + num2 + " Equals " + addresult);
+                System.out.println("\n\nImagine you have " + num1 + " candies in one hand and " + num2 + " candies in your other hand. If you put them all together, you have " + addresult + " candies in total.");
+                System.out.println("In math words: " + num1 + " plus " + num2 + " equals " + addresult);
                 break;
 
 
             case 2:
                 System.out.println("\nSubtraction selected!\nNOTE: Number 1 must be bigger than Number 2");
 
-                num1 = 0;
-                num2 = 0;
-                validAddInput = false;
-                validAddInput2 = false;
-
+               reset();
 
                 validNumber();
                 validNumber2();
+
+                if(num2 > num1){
+                    System.out.println("Number 1 must be bigger than Number 2!");
+                    reset();
+
+                    validNumber();
+                    validNumber2();
+                }
                 int subresult = num1 - num2;
 
                 for (int i = 1; i <= num1; i++) {
@@ -195,20 +201,30 @@ public class KidsCalc {
                     System.out.print("+-----+ ");
                 }
 
-                System.out.println("\n\n As we can see: " + num1 + " - " + num2 + " Equals " + subresult);
+                System.out.println("\n\nLet's say you start with " + num1 + " stickers, but you give " + num2 + " stickers to your friend. Now, you only have " + subresult + " stickers left.");
+                System.out.println("In math words: " + num1 + " minus " + num2 + " equals " + subresult);
+
                 break;
 
             case 3:
                 System.out.print("\nDivision selected!\n");
-                num1 = 0;
-                num2 = 0;
-                validAddInput = false;
-                validAddInput2 = false;
+                reset();
                 validNumber();
                 validNumber2();
+
                 int divresult = num1 / num2;
+                int divremaninder = num1 % num2;
+                while(divremaninder != 0){
+                    System.out.println(num1+" does not evenly divide into "+num2+". It has a remainder of "+divremaninder+".\nYou could try "+(num1-divremaninder)+" divided by "+(num2)+"?");
+                    reset();
+                    validNumber();
+                    validNumber2();
+                    divresult = num1 / num2;
+                    divremaninder = num1 % num2;
+                }
 
                 int x = 1;
+                int y = 1;
                 while (x < (divresult)) {
 
                     for (int i = 0; i < num2; i++) {
@@ -220,7 +236,8 @@ public class KidsCalc {
                     }
                     System.out.println("+-----+ ");
                     for (int i = 0; i < num2; i++) {
-                        System.out.print("|  " + x + "  | ");
+                        System.out.print("|  " + y + "  | ");
+                        y++;
                     }
                     System.out.print("                                         ");
                     for (int i = 0; i < num2; i++) {
@@ -254,7 +271,8 @@ public class KidsCalc {
                 System.out.println("+-----+ ");
 
                 for (int i = 0; i < num2; i++) {
-                    System.out.print("|  " + divresult + "  | ");
+                    System.out.print("|  " + y + "  | ");
+                    y++;
                 }
                 System.out.print("      DIVIDED BY      ");
 
@@ -278,21 +296,22 @@ public class KidsCalc {
                 System.out.print("                   ");
                 System.out.println("+-----+ ");
 
-                System.out.println("\n\n As we can see,if person 1 shares " + num1 + " apples with " + num2 + " people,then everyone gets " + divresult + " apples");
-                System.out.println("In maths words: " + num1 + " divided by " + num2 + " equals " + divresult);
+                System.out.println("\n\nImagine you have " + num1 + " cookies and " + num2 + " friends, and you want to share the cookies equally with all your friends. Each friend gets " + divresult + " cookies.");
+                System.out.println("In math words: " + num1 + " divided by " + num2 + " equals " + divresult);
+
+
 
                 break;
 
             case 4:
                 System.out.print("\nMultiplication selected!\n");
-                num1 = 0;
-                num2 = 0;
-                validAddInput = false;
-                validAddInput2 = false;
+                reset();
                 validNumber();
                 validNumber2();
+                int multresult = num1 * num2;
 
                 int z = 1;
+                int a =1;
                 while (z < num1) {
 
 
@@ -312,9 +331,19 @@ public class KidsCalc {
                     for (int i = 0; i < num2; i++) {
                         System.out.print("        ");
                     }
-                    for (int i = 1; i <= (num2); i++) {
-                        System.out.print("|  " + i + "  | ");
-                    }
+
+                    int b;
+                    b=1 +(num2 *(z-1));
+
+                        for (a = 1; a <= num2; a++) {
+                            if(b<10){
+                            System.out.print("|  " + b + "  | ");}
+                            if(b>=10){
+                                System.out.print("|  " + b + " | ");
+                            }
+                            b++;
+                        }
+
                     System.out.println();
                     System.out.print("+-----+ ");
                     System.out.print("                                         ");
@@ -344,9 +373,18 @@ public class KidsCalc {
                     System.out.print("|  " + i + "  | ");
                 }
                 System.out.print("     EQUALS     ");
+
+                int c =multresult-num2+1;
                 for (int i = 1; i <= num2; i++) {
-                    System.out.print("|  " + i + "  | ");
+                    if(c<10){
+                        System.out.print("|  " + c + "  | ");}
+                    if(c>=10){
+                        System.out.print("|  " + c + " | ");
+                    }
+                    c++;
+
                 }
+
                 System.out.println();
                 System.out.print("+-----+                          ");
                 for (int i = 0; i < num2; i++) {
@@ -356,7 +394,8 @@ public class KidsCalc {
                 for (int i = 0; i < num2; i++) {
                     System.out.print("+-----+ ");
                 }
-                System.out.println();
+                System.out.println("\n\nThink about having " + num1 + " boxes, and each box has " + num2 + " chocolates. All together, that's " + multresult + " chocolates!");
+                System.out.println("In math words: " + num1 + " times " + num2 + " equals " + multresult);
 
                 break;
             case 0:
