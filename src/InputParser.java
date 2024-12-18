643a469efaba1ce326151021bc54c8427d3fbae3
U3RhcnQ <165941707+U3RhcnQ@users.noreply.github.com>
2024-12-18 16:38:51 +0000

    Major Fix ?

--- src/InputParser.java
+++ src/InputParser.java
@@ -39,6 +39,7 @@ public class InputParser {
                     if (!operators.contains(lastCharacter) && !lastCharacter.equals("(") && !lastCharacter.equals("-(")) {
                         throw new PrettyException("It looks like you have a bracket and a number beside each other with no operator", expression, i);
                     }
+                    openBracketCount++;
 
                 }else if (i == 0 && c.equals(")")) {
                     throw new PrettyException("It looks like you have an closing bracket before any number", expression, i);
@@ -49,10 +50,12 @@ public class InputParser {
                         throw new PrettyException("It looks like you have a closing Bracket before a opening one", expression, i);
                     } else {
                         openBracketCount--;
+                        System.out.println("Open Bracket -1: "+ openBracketCount);
                     }
 
                 } else if (c.equals("(")) {
                     openBracketCount++;
+                    System.out.println("Open Bracket +1: "+ openBracketCount);
 
                 } else if (i == 0 && !c.equals("-")) {
                     throw new PrettyException("It looks like you have a operator "+c+" at the start of your expression with no number before it", expression, i);
