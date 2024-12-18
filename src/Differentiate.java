6591df561222cfd53d2730b25f02ce45f753b52e
Ciaran Lynch <lynchbros3@gmail.com>
2024-12-18 10:48:36 +0000

    commented Differentiate

--- src/Differentiate.java
+++ src/Differentiate.java
@@ -3,28 +3,35 @@ import java.util.List;
 
 public class Differentiate extends Calculator{
     private static String inputEquation;
-    List<String> orderOfOperations = List.of("*","/","+","-","(",")");
+    List<String> orderOfOperations = List.of("*","/","+","-","(",")"); // List of the operands that include round brackets to make the parser treat these differently
 
+    // Constructor of the Differentiate object that takes in the user input and gives it's value to inputEquation
     public Differentiate(String expression) {
         super(expression);
         this.inputEquation = expression;
     }
-
+    // Override the abstract method parse() from Calculator.java
     @Override
     public void parse() throws Exception {
-        setExpressionList(InputParser.parseFromString(inputEquation, orderOfOperations, "x+-*/^="));
+        setExpressionList(InputParser.parseFromString(inputEquation, orderOfOperations, "x+-*/^=")); // Method from Calculator that sets the Arraylist ExpressionList
+        // parseFromString is a method from InputParser that takes a string and converts to an ArrayList of elements and operands
     }
 
+    // Override the abstract method solve() from Calculator.java
     @Override
     public String solve() throws Exception {
         parse();
-        ArrayList<String> elementList = getExpressionList();
+        ArrayList<String> elementList = getExpressionList(); // create a variable to store the Expression List
 
+        // Iterate through the arraylist
+        // differentiate the elements and add them to the string equation
+        // add the operands to the string equation
         String equation = "";
         for (int index = 0; index<elementList.size(); index++){
             String item = elementList.get(index);
             if (orderOfOperations.contains(item)) {
                 equation = equation + item;
+                // if a bracket is found throw an exception to tell the user off
                 if (item.equals("(") || item.equals(")")){
                     throw new PrettyException("It looks like you hava a bracket that isn't supposed to be there",inputEquation,index);
                 }
@@ -35,6 +42,7 @@ public class Differentiate extends Calculator{
         System.out.println();
         System.out.printf("This leaves us with the equation: %s%n",equation);
 
+        // check is the last character in equation an operand if so remove it
         char lastChar = equation.charAt(equation.length()-1);
         if (!Character.isDigit(lastChar) && !String.valueOf(lastChar).equals("x")){
             System.out.println();
@@ -46,6 +54,7 @@ public class Differentiate extends Calculator{
     }
 
     private String differentiate(String expression){
+        // if the element has a power then multiply the coefficient of the x by the power and reduce the power by 1
         if (expression.contains("^")) {
             int index = expression.indexOf("^");
             int number = Integer.parseInt(expression.substring(0, index-1));
@@ -57,9 +66,10 @@ public class Differentiate extends Calculator{
             }else{
                 expression = String.valueOf(number) + expression.substring(index-1, index+1) + String.valueOf(power);
             }
-        }else{
+        // if the element has just an x with no power then remove the x
+        }else if (expression.contains("x")){
             expression = expression.substring(0,expression.length()-1);
         }
-        return expression;
+        return expression; // return the new element
     }
 }
